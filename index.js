907f51d46ee236da4380ea93984b84de7baeaaed
TeaWhyDee <daniel.555365@gmail.com>
2021-06-03 16:24:41 +0300

    mongodb

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const { Telegraf } = require('telegraf')
 
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+
+const url = 'mongodb://localhost:27017';
+
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+    if (err) throw err;
+    console.log(client.topology.clientInfo);
+    client.close();
+    const db = client.db("testdb");
+    db.listCollections().toArray().then((docs) => {
+        console.log('Available collections:');
+        docs.forEach((doc, idx, array) => { console.log(doc.name) });
+    }).catch((err) => {
+        console.log(err);
+    }).finally(() => {
+        client.close();
+    });
+});
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
